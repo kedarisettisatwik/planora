06748332168b4b4e54f12b41b6358a0d9238398f
kedarisetti satwik <satwik1330@gmail.com>
2026-07-18 08:37:13 +0530

    toastify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Log from './pages/Log';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Log />} />
-      <Route path="/log" element={<Log />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    <>
+      <ToastContainer 
+      position="top-center"
+      autoClose={2000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick={true} />
+      <Routes>
+        <Route path="/" element={<Log />} />
+        <Route path="/log" element={<Log />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/Log.js
+++ src/pages/Log.js
@@ -1,15 +1,20 @@
 
 // import { useNavigate } from 'react-router-dom';
 import { useState, useEffect, useRef } from 'react';
+ import { toast } from 'react-toastify';
 import './Log.css';
 import login_image from '../assests/login.png';
 import signup_image from '../assests/signup.png';
 import reset_image from '../assests/reset_password.png';
+import eye_hide from '../assests/eye_hide.png';
+import eye_show from '../assests/eye_show.png';
 
 function Log() {
   // const navigate = useNavigate();
   const [activeForm, setActiveForm] = useState('login');
   const [contentVisible, setContentVisible] = useState(false);
+
+  const [show_password,setShow_password] = useState('hide');
 
   const headingRef = useRef(null);
 
@@ -68,6 +73,35 @@ function Log() {
     };
   }, []);
 
+  // Login form state
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  // Signup form state
+  const [signupName, setSignupName] = useState('');
+  const [signupEmail, setSignupEmail] = useState('');
+  const [signupPassword, setSignupPassword] = useState('');
+  const [signupConfirmPassword, setSignupConfirmPassword] = useState('');
+
+  // Reset form state
+  const [resetEmail, setResetEmail] = useState('');
+
+  const LoginSubmitForm = (event) => {
+    event.preventDefault();
+
+    if (loginEmail.length == 0 || loginPassword.length == 0){
+      toast("All Fields Required *",{
+        style: { background: 'var(--toast_error)', color: '#fff',letterSpacing:'1px' },
+      });
+      return ;
+    }
+
+    toast("submitted",{
+      style: { background: 'var(--toast_success)', color: '#fff',letterSpacing:'1px' },
+    });
+
+  }
+
   return (
     <section className='log section'>
       <main className='main'>
@@ -76,15 +110,19 @@ function Log() {
             <p className='description'>Your productivity hub starts here — sign in to take control of your day</p>
 
             {activeForm === 'login' && (<div className='logs loginForm'>
-              <form>
+              <form onSubmit={LoginSubmitForm}>
                   <h3>Hey there, ready to log in? </h3>
                   <div className='input_box'>
                     <label>Email</label>
-                    <input type='email' placeholder='example@gmail.com' autocomplete="off"></input>
+                    <input type='email' placeholder='example@gmail.com' autoComplete="off" value={loginEmail}
+                      onChange={(e) => setLoginEmail(e.target.value)}></input>
                   </div>
-                  <div className='input_box'>
+                  <div className={`input_box ${show_password}`}>
                     <label>Password</label>
-                    <input type='password' placeholder='********' autocomplete="off"></input>
+                    <input type={`${show_password === 'show' ? 'text' : 'password'}`} placeholder='********' autoComplete="off" value={loginPassword}
+                      onChange={(e) => setLoginPassword(e.target.value)}></input>
+                    <img src={eye_hide} className='eye_hide' onClick={() => setShow_password('hide')}></img>
+                    <img src={eye_show} className='eye_show' onClick={() => setShow_password('show')}></img>
                   </div>
                   <input type='submit' value='Log In' className='submit'></input>
                   <p>New to Planora ? Click here - <span onClick={() => setActiveForm('signup')}>Signup</span></p>
@@ -99,19 +137,23 @@ function Log() {
                   <h3>Let’s Get You Started</h3>
                   <div className='input_box'>
                     <label>Name</label>
-                    <input type='text' placeholder='Hi Mr / Ms ?' autocomplete="off"></input>
+                    <input type='text' placeholder='Hi Mr / Ms ?' autoComplete="off" value={signupName}
+                      onChange={(e) => setSignupName(e.target.value)}></input>
                   </div>
                   <div className='input_box'>
                     <label>Email</label>
-                    <input type='email' placeholder='example@gmail.com' autocomplete="off"></input>
+                    <input type='email' placeholder='example@gmail.com' autoComplete="off" value={signupEmail}
+                      onChange={(e) => setSignupEmail(e.target.value)}></input>
                   </div>
                   <div className='input_box'>
                     <label>Password</label>
-                    <input type='password' placeholder='********' autocomplete="off"></input>
+                    <input type='password' placeholder='********' autoComplete="off" value={signupPassword}
+                      onChange={(e) => setSignupPassword(e.target.value)}></input>
                   </div>
                   <div className='input_box'>
                     <label>Confirm Password</label>
-                    <input type='text' placeholder='********' autocomplete="off"></input>
+                    <input type='text' placeholder='secret ***' autoComplete="off" value={signupConfirmPassword}
+                      onChange={(e) => setSignupConfirmPassword(e.target.value)}></input>
                   </div>
                   <input type='submit' value='Sign Up' className='submit'></input>
                   <p>Already have account ? Click here - <span onClick={() => setActiveForm('login')}>Log In</span></p>
@@ -125,9 +167,10 @@ function Log() {
                   <h3>Forgot Password ? Don't worry</h3>
                   <div className='input_box'>
                     <label>Email</label>
-                    <input type='email' placeholder='example@gmail.com' autocomplete="off"></input>
+                    <input type='email' placeholder='example@gmail.com' autoComplete="off"></input>
                   </div>
-                  <input type='submit' value='Reset Password' className='submit'></input>
+                  <input type='submit' value='Reset Password' className='submit' value={resetEmail}
+                      onChange={(e) => setResetEmail(e.target.value)}></input>
                   <p>Back to Login Page ? Click here - <span onClick={() => setActiveForm('login')}>LogIn</span></p>
               </form>
               <img src={reset_image} className='reset_image img'></img>
