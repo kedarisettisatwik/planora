c8101542fabec8b82408ddf87122d6d3a16c29b3
kedarisetti satwik <satwik1330@gmail.com>
2026-07-19 07:11:51 +0530

    list

--- src/components/NoWidgets.js
+++ src/components/NoWidgets.js
@@ -6,11 +6,22 @@ import '../Styles/Home.css';
 function NoWidgets({ setWidgetsCount, Signout, email, displayName }){
     return (
         <section className="addWidget">
-            <h1><span>Hello,</span> {displayName} </h1>
-            <p className='description'>No widgets yet! Start building your productivity hub by adding one now.</p>
+            <h1><span>Hello,</span> <br></br> {displayName} </h1>
+            <p className='description'>No widgets yet! Start building your productivity hub by adding one now.<br></br><br></br>select one...</p>
 
-            <button className="addBtn" onClick={() => setWidgetsCount(prevCount => prevCount + 1)}><i className="fa-solid fa-plus"></i> Add Widget</button>
-            <button onClick={Signout}>Sign Out</button>
+            <ul>
+                <li>Daily Goals</li>
+                <li>Things to Do</li>
+                <li>Meetings / Events Reminder</li>
+                <li>Dairy</li>
+                <li>Sticky Notes</li>
+                <li>Track Expenses</li>
+                <li>Book Library</li>
+                <li>Track Project</li>
+                <li>Teams</li>
+            </ul>
+
+            <button onClick={Signout} className="exit">Log Out</button>
         </section>
     );
 }
