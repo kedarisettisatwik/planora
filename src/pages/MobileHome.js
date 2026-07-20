c5146b2be73ac0a6f8a774e1d2f7336c5d28420e
kedarisetti satwik <satwik1330@gmail.com>
2026-07-20 05:32:37 +0530

    mobile msg

--- src/pages/MobileHome.js
+++ src/pages/MobileHome.js
@@ -1,7 +1,7 @@
 
 function MobileHome(){
     return(
-        <p style={{margin:"auto 0"}}>Planora is still not available on Mobile devices, please use Desktop <br></br> <br></br> we will be comming soon.</p>
+        <p style={{margin:"auto 0",padding:"0px 20px"}}>Planora is still not available on Mobile devices, please use Desktop <br></br> <br></br> we will be comming soon.</p>
     )
 }
 
